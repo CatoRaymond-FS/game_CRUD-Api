4eb875ff669f729ad1c99982edabd1fac0a60758
CatoRaymond-FS <97109752+CatoRaymond-FS@users.noreply.github.com>
2022-12-05 22:10:36 -0500

    add full crud

--- reactjs/src/App.js
+++ reactjs/src/App.js
@@ -1,50 +1,17 @@
-import React, { useState, useEffect } from 'react';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
+import Home from './pages/Home';
+import Dashboard from './pages/Dashboard';
+import Game from './pages/Game';
 function App() {
-  const [games, setGames] = useState(null);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
-
-
-  const API_BASE = process.env.NODE_ENV === 'development' 
-  ? `http://localhost:8000/api/v1` 
-  : process.env.REACT_APP_BASE_URL;
-
-  let ignore = false;
-  useEffect(() => {
-
-
-    if(!ignore){
-      getGames();
-    }
-
-    return () => { ignore = true; }
-  }, [])
-
-  const getGames = async () => {
-    try{
-      await fetch(`${API_BASE}/games/`)
-          .then(response => response.json())
-          .then(data => {
-            console.log({data})
-            setGames(data)
-      })
-    } catch(error){
-      setError(error.message || "Unexpected Error Occurred!")
-    } finally {
-      setLoading(false)
-    }
-  }
   return (
-    <div className="App">
-      <header className="App-header">
-       <h1>Games:</h1>
-        <ul>
-          <li>Games</li>
-        </ul>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/game/:id" exact element={<Game />} />
+        <Route path="/dashboard" exact element={<Dashboard />} />
+        <Route path="/" exact element={<Home />} />
+      </Routes>
+    </Router>
   );
 }
 
